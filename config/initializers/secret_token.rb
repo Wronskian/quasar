0b51c114c89e702e538b30de6d4b20617feb96d7
Ryan Dalke <rdalke@gmail.com>
2011-02-25 09:34:09 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quasar::Application.config.secret_token = '4f990a2ff6eea19bbf66c3255f87505e313b4e18a55b7fb9a070e6555f1f43137f7cc5076abaa1526f81852691f576cdae42dc7d6e0ba26a9cdc8b1d21006855'
